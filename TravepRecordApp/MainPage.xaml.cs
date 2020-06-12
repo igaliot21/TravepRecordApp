8e633a9e5bba1262a7cd857cef18e8a22d9be40f
Jose Mª Izquierdo Galiot <igaliot21@gmail.com>
2020-06-12 15:14:32 +0200

    Agregar archivos de proyecto.

---
+++ TravepRecordApp/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TravepRecordApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void LogInButton_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(entUser.Text)) { }
+            if (string.IsNullOrEmpty(entPassword.Text)) { }
+        }
+    }
+}
